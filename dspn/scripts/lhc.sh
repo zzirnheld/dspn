02d11fd5a87740d4330a9f3fcd787f7b29e7a7bd
Eimti <eimtixa@gmail.com>
2021-04-13 11:49:53 -0400

    only 1 epoch for plotting

--- dspn/scripts/lhc.sh
+++ dspn/scripts/lhc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ARGS="--show --loss chamfer --encoder FSEncoder --lr 0.01 --dim 256 --dataset mnist --epochs 100 --latent 64 --mask-feature --inner-lr 800"
-ARGS="--loss hungarian --encoder FSEncoder --lr 0.01 --dim 256 --dataset lhc --epochs 5 --batch-size 16 --latent 64 --mask-feature --inner-lr 800"
+ARGS="--loss hungarian --encoder FSEncoder --lr 0.01 --dim 256 --dataset lhc --epochs 1 --batch-size 16 --latent 64 --mask-feature --inner-lr 800"
 
 # DSPN train
 python3 train.py $ARGS --decoder DSPN --name dspn-lhc
